f851e7efb724570fff9a4c1d32b2f39e5a2a6581
PhilRob <ph.robinet@gmail.com>
2018-12-06 23:45:46 +0100

    mise à jour éléphant

--- ExLongest.js
+++ ExLongest.js
@@ -1,11 +1,13 @@
-const words = ['chien', 'chat', 'lapin', 'chevaux']
-const result =[]
-let longestWord =''
+const words = ['chien', 'chat', 'lapin', 'chevaux', 'éléphant'];
+const result =[];
+
+let longestWord ='';
+
 for ( const word of words) {
     if(word.length > longestWord.length) {
-        longestWord = word
-        console.log(longestWord)
+        longestWord = word;
+        // console.log(longestWord);
     }
 }
-result.push (longestWord)
+result.push (longestWord);
 console.log(result)
