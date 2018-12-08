e5d5499967558e76d2efbc4b15644ab715718267
PhilRob <ph.robinet@gmail.com>
2018-12-08 13:34:43 +0100

    Extraction du mot le ou les + long

--- ExLongest.js
+++ ExLongest.js
@@ -1,13 +1,30 @@
-const words = ['chien', 'chat', 'lapin', 'chevaux', 'éléphant'];
+const words = ['chien', 'chat', 'lapin', 'chevaux', 'canard', 'oiseaux', 'éléphant'];
 const result =[];
 
 let longestWord ='';
 
+
 for ( const word of words) {
-    if(word.length > longestWord.length) {
+    if(word.length >= longestWord.length) {
         longestWord = word;
-        // console.log(longestWord);
+        console.log(longestWord);
+    } 
+}
+console.log(longestWord.length);
+
+let mot = longestWord.length;
+
+for (const w of words) {
+    if (w.length === mot){
+        result.push(w);
     }
 }
-result.push (longestWord);
-console.log(result)
+console.log(result);
+
+
+
+// result.push (longestWord);
+// console.log(result);
+// const a = 'chien';
+// const b = a.length;
+// console.log(b);
