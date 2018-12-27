b32ac9fb2de1ab0395a8a7822631814e732d175d
PhilRob <ph.robinet@gmail.com>
2018-12-27 10:56:20 +0100

    Mise a jour de Double.js

--- Double.js
+++ Double.js
@@ -1,12 +1,18 @@
-const duplicate = ['foobar', 'test', 'john', 'doe', 'foobar', 'test']
-duplicate.sort();
+const duplicate = ['foobar', 'test', 'john', 'doe','test',  'foobar', 'test', 'test', 'test',  'foobar', 'test',  'foobar', 'test', 'fin']
+var tableau =[];
+for (var i = 0 ; i<= duplicate.length; i++){
+    tableau[i] = duplicate[i];
+};
+console.log(tableau);
+var max = tableau.length;
 
-
-for(var i = 0; i <= duplicate.length ; i++){
-    if(duplicate[i] === duplicate[i + 1]){
-        var elementSupp = duplicate.splice(i, 1);
-            console.log(duplicate);
-        }
+for(max; max >= 1; max--){
+    tableau.sort();
+    if(tableau[max] === tableau[max - 1]){
+        var elementSupp = tableau.splice(max, 1);
+        console.log(tableau);
+    }
 }
 
-alert('Voici le  tableau sans doublon : ' + duplicate);
+console.log('le tableau duplucate : ' +duplicate);
+alert('Voici le  tableau sans doublon : ' + tableau);
